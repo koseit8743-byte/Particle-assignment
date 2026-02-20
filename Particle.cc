f4273fd389ac53247dfdb7fddb62128fd7869c8f
koseit9001 <koseit8743@gmail.com>
2026-02-19 16:47:23 -0800

    Fixed compiler error typo

--- Particle.cc
+++ Particle.cc
@@ -1,8 +1,6 @@
 #include "Particle.h"
 
 #include <cstdint>
-#include <numeric>
-#include <sys/types.h>
 Particle::Particle(float row1, float col1, ParticleType Type){
 	row = row1;
 	col = col1;
@@ -26,28 +24,7 @@ ParticleType Particle::getType() const {
 bool Particle::isStill() const{
 return still;
 }
-
-float Particle::getXvel() const {
-	return x_vel;
-}
-
-float Particle::getYvel() const {
-	return y_vel;
-}
-int Particle::getLifetime() const {
-	return lifetime;
-}
-
-void Physics(World& physics) {
-	float x_vel = 0;
-	float y_vel = -9.8; //this should default to gravity (otherwise user can eidt it)
-	}
-
-void Particle::getColor( uint8_t & r, uint8_t & g, uint8_t & b) const{
-r = red;
-g = green;
-b  = blue;
-}
+float Particle::
 void Particle::setRow(float r, float c) {
 	row = r;
 	col = c;
@@ -56,9 +33,5 @@ void Particle::setRow(float r, float c) {
 void Particle::setStill(bool isStill){
 	still = isStill;
 }
-void Particle::setLifetime(int frames){ lifetime = frames;}
-void Particle::setColor(uint8_t Red, uint8_t Green, uint8_t Blue){
-	red = Red;
-	green = Green;
-	blue = Blue;
-}
+
+
