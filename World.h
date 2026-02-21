1458be6dfb50e3157378da5e69857b1d35cea5e5
yousaf_1004564 <1004564@my.scccd.edu>
2026-02-20 23:16:38 -0800

    updated World.cc will work on istouching

--- World.h
+++ World.h
@@ -11,20 +11,26 @@ class World {
 	int columns = 0;
 	vector<vector<char>> World_Map;
 	list<Particle> Parts;
+	list<Particle> Part_World;
  	public: 
 //	World(int World_rows, int World_columns) : rows(World_rows), columns(World_columns) {};	
 	World();	
 	friend class World_Particles;
 	void GetRows() const;
 	void GetColumns() const;
-	void SetrRows(int World_rows);
-	void SetColumns(int World_columns);
+	void SetRows(int World_rows) {
+		rows = World_rows;
+	}
+	void SetColumns(int World_columns) { 
+		columns = World_columns;
+	}
 
-	
 	char get_Map_Location(vector<vector<char>> &World_Map, size_t rows, size_t columns);
 
+	int size() const;
+	int alive_count();
 	void Boundaries();		
-
+	bool isEmpty(int rows, int columns);
 	void Game_Map();		
 };
 //If the particle goes out of bounds -> call a destructor on it or something
